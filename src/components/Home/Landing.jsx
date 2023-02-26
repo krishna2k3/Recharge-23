d9275a52b85ebcb699c9254a757854d94489a042
krishna2k3 <krishna101ravi@gmail.com>
2023-02-26 19:05:06 +0530

    "... opens soon" - "... passion!"

--- src/components/Home/Landing.jsx
+++ src/components/Home/Landing.jsx
@@ -19,12 +19,12 @@ const Landing = () => {
             alt=""
           />
           <h2 className="font-semibold  text-2xl">
-            RECHARGE YOUR SPIRIT AND REIGNITE YOUR PASSION
+            RECHARGE YOUR SPIRIT AND REIGNITE YOUR PASSION!
           </h2>
           <h2 className="font-semibold text-2xl ">23rd - 25th MARCH, 2023</h2>
           <Countdown className=""/>
           <button className="border px-4 py-2 rounded-md font-medium my-4 hover:bg-white hover:text-black hover:duration-500">
-            REGISTRATIONS SOON
+            REGISTRATION OPENS SOON
           </button>
         </div>
         <div className="flex flex-row justify-center items-center gap-x-2">
