c565d3da3a268e8d4dec71ac0da91eaec40ce5a8
c0d3-br3ak9r <103585603+c0d3-br3ak9r@users.noreply.github.com>
2023-02-26 19:54:58 +0530

    Added event cards filtering animation

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,7 +1,13 @@
+import { motion } from "framer-motion";
+
 const EventCard = ({title}) => {
-    return <div className="h-40 w-40 rounded-md bg-slate-300 mb-4">
-            <p className="text-3xl p-4">{title}</p>
-        </div>
+    return <motion.div layout
+            animate={{opacity: 1, scale: 1}}
+            initial={{opacity: 0, scale: 0}}
+            exit={{opacity: 0, scale: 0}}
+            className="h-96 w-60 rounded-md bg-slate-300 m-10 hover:!scale-105 duration-500">
+            <p className="text-3xl p-4 text-center">{title}</p>
+        </motion.div>
 };
 
 export default EventCard;
