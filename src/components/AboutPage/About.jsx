72536d5fb22bebdc12980489018fd4371403dfea
Subze23 <subzesubhashsanth@gmail.com>
2023-02-27 20:30:09 +0530

    added footer files and minor changes

--- src/components/AboutPage/About.jsx
+++ src/components/AboutPage/About.jsx
@@ -4,8 +4,8 @@ const AboutPage = ()=>{
             <div id="about" className="flex flex-col lg:flex-row stretch-to-screen bg-[#1A1330] text-white items-center">
             <div id="left" className="basis-1/2">
                 <p className="ml-10 mt-16 text-7xl lg:mt-16 lg:ml-24">About</p>    
-                <hr class="ml-10 lg:ml-24 h-px w-20 my-8 bg-gray-200 border-2 dark:bg-gray-700"></hr>
-                <hr class="ml-24 lg:ml-36  w-20 my-8 bg-gray-200 border-2 dark:bg-gray-700"></hr>
+                <hr class="ml-10 lg:ml-24 border-0 h-1 w-20 mt-2 bg-[#ffd54f]"></hr>
+                <hr class="ml-24 lg:ml-36 w-20 h-1 border-0 mt-4 bg-[#ffd54f]"></hr>
                 <p className="mx-10 my-10 lg:mx-24 ">
                     A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. Though not required by the orthographic conventions of any language with a writing system, paragraphs are a conventional means of organizing extended segments of prose. Wikipedia
                 </p>
--- src/components/AboutPage/AboutContent.jsx
+++ src/components/AboutPage/AboutContent.jsx
@@ -1,6 +1,6 @@
 const AboutContent = ()=>{
     return (
-        <div className="stretch-to-screen items-center">
+        <div className="stretch-to-screen bg-[#1A1330] items-center">
             <div className="mt-10 flex flex-col gap-y-5 items-center text-center ">
                 <img
                     src={require("../../assets/recharge-logo.png")}
@@ -8,7 +8,7 @@ const AboutContent = ()=>{
                     alt=""
                 />
             </div>
-            <p className="mx-10 my-10 lg:my-16 lg:mx-52 text-lg justify-between">
+            <p className="mx-10 my-10 lg:my-16 lg:mx-52 text-lg text-white justify-between">
                 Rajalakshmi Institutions is one of the premier Educational Institutions in the state. The institution offers various streams of study from Architecture, Engineering, Management to Nursing. From securing university level ranks to making our mark in National and International events, the students of this Institution have brilliant academic credentials. At Rajalakshmi Institutions, we strongly believe that every student should be given ample opportunities to chase their dreams. Our motto has always been "To provide technically competent professionals with character and humane values to the society."
                 <br></br><br></br>At Rajalakshmi Institutions, the students are encouraged to excel not only in their academics but also in their co-curricular and extracurricular activities. The institutions believes that learning can happen outside the classroom through engagement with creative arts. The students are also encouraged to positively influence the society they live in through community engagement. This is done through the various societies for music (UNISON), literature and debate (Atrium and Debating Union), social services (NSS), Rotaract and several other student enterprises/forums that the institutions host. All this make the students passing out of this institutions a great value addition to any organisation they are a part of.
                 <br></br><br></br>Recharge 2023 has been conceptualised to be the flagship Cultural Event of Rajalakshmi institutions. The team at Recharge 2023 have been working tirelessly to bring about a truly memorable Intercollegiate cultural fest to all participants. The festival will serve as a platform for budding young artists to showcase their talent to the best of the judges from their respective fields. It's said that food and fun are synonymous, well at Recharge we promise you both. Our amazing lineup of food stalls and events stand testimony to that. At the end of the day any intercollegiate event happens to be the best place to meet new people and make new memories. The Recharge team is fully committed to delivering a truly best in class cultural fest.
