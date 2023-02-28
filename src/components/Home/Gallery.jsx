bca6f4fb6b2ce049c24fc6aead27dee08a45a476
manosanjay-g <manosanjay.web@gmail.com>
2023-03-01 05:12:28 +0530

    changed the padding

--- src/components/Home/Gallery.jsx
+++ src/components/Home/Gallery.jsx
@@ -33,7 +33,7 @@
 //         }
 
 //     ]
-    
+
 //     return (
 //         <div id="about" className="stretch-to-screen bg-[#06111f] text-white items-center">
 //                 <p className='my-6 text-7xl'>Gallery</p>
@@ -54,53 +54,57 @@
 // export default Gallery
 
 const Gallery = () => {
-    return(
-        <div id="about" className="stretch-to-screen bg-[#06111f] text-white items-center">
-            <p className='mb-6 ml-8 pt-6 text-7xl'>Gallery</p>
-        <div class="flex flex-col lg:flex-row pb-6 mx-4">
-            <div class="flex flex-col mb-0">
-            <img
+  return (
+    <div
+      id="about"
+      className="stretch-to-screen bg-[#06111f] text-white items-center px-40 py-10"
+    >
+      <p className="text-7xl font-extrabold shadow-inner">Gallery</p>
+      <hr className=" border-2 w-20 my-4 ml-2 border-red-500"></hr>
+      <hr className="w-20 my-4 border-2 ml-8 border-red-500 "></hr>
+      <div class="flex flex-col lg:flex-row pb-6 my-6">
+        <div class="flex flex-col mb-0">
+          <img
             src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
             className="w-100 shadow-lg rounded mb-4 hover:opacity-75 duration-700"
             alt="Boat on Calm Water"
-            />
-    <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
-      className="w-100 shadow-lg  rounded mb-4 hover:opacity-75 duration-700"
-      alt="Wintry Mountain Landscape"
-    />
-  </div>
-
-  <div className="flex flex-col lg:mx-3 mb-0">
-    <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
-      className="w-100 shadow-lg rounded mb-4 hover:opacity-75 duration-700"
-      alt="Mountains in the Clouds"
-    />
-
-    <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-      className="w-100 shadow-lg rounded mb-4 hover:opacity-75 duration-700"
-      alt="Boat on Calm Water"
-    />
-  </div>
-
-  <div className="flex flex-col mb-0">
-    <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
-      className="w-100 shadow-lg rounded mb-4 hover:opacity-75 duration-700"
-      alt="Waves at Sea"
-    />
-
-    <img
-      src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
-      className="w-100 shadow-lg rounded mb-4 hover:opacity-75 duration-700"
-      alt="Yosemite National Park"
-    />
-  </div>
-</div>
-
+          />
+          <img
+            src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
+            className="w-100 shadow-lg  rounded mb-4 hover:opacity-75 duration-700"
+            alt="Wintry Mountain Landscape"
+          />
         </div>
-    )
-}
-export default Gallery
+
+        <div className="flex flex-col lg:mx-3 mb-0">
+          <img
+            src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
+            className="w-100 shadow-lg rounded mb-4 hover:opacity-75 duration-700"
+            alt="Mountains in the Clouds"
+          />
+
+          <img
+            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+            className="w-100 shadow-lg rounded mb-4 hover:opacity-75 duration-700"
+            alt="Boat on Calm Water"
+          />
+        </div>
+
+        <div className="flex flex-col mb-0">
+          <img
+            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
+            className="w-100 shadow-lg rounded mb-4 hover:opacity-75 duration-700"
+            alt="Waves at Sea"
+          />
+
+          <img
+            src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
+            className="w-100 shadow-lg rounded mb-4 hover:opacity-75 duration-700"
+            alt="Yosemite National Park"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Gallery;
