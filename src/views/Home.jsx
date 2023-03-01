3d90b18301825acd07445120ca07f0e68227c519
Subze23 <subzesubhashsanth@gmail.com>
2023-03-01 13:13:24 +0530

    minor changes

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,7 +4,7 @@ import Gallery from "../components/Home/Gallery";
 import AboutRecharge from "../components/Home/AboutRecharge";
 import ProShows from "../components/Home/ProShows";
 import Sponsors from "../components/Home/Sponsors";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { motion } from "framer-motion";
 import AboutREC from "../components/Home/AboutREC";
 import NavBar from "../components/Home/NavBar";
@@ -17,7 +17,19 @@ const Home = ({ isLoading, setLoading }) => {
     setLoading(false);
   }, 1900);
 
+  const [activeComponent, setActiveComponent] = useState(1);
+
+  const scrollToComponent = (direction) => {
+    if (direction === 'forward') {
+      setActiveComponent(activeComponent + 1);
+    } else if (direction === 'backward') {
+      setActiveComponent(activeComponent - 1);
+    }
+  };
+
+
   return (
+    
     <div>
       {isLoading === true ? (
         <Loading />
@@ -27,11 +39,16 @@ const Home = ({ isLoading, setLoading }) => {
           animate={{ opacity: 1, transition: { duration: 1.5 } }}
           exit={{ opacity: 0 }}
           className="snap-container"
+          onWheel={(event) => {
+            const direction = event.deltaY > 0 ? 'forward' : 'backward';
+            scrollToComponent(direction);
+          }}
         >
-          <Landing />
-          <AboutRecharge />
-          <AboutREC />
-          <Gallery />
+          {activeComponent === 1 && <Landing />}
+          {activeComponent === 2 && <AboutRecharge />}
+          {activeComponent === 3 && <AboutREC />}
+          {activeComponent === 4 && <Gallery />}
+                   
           {/* <ProShows />
           <Sponsors /> */}
         </motion.div>
