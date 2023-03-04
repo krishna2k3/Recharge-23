82b52845c1b3f90bec51d787683bed07a64666db
Subze23 <subzesubhashsanth@gmail.com>
2023-03-04 21:25:26 +0530

    font update

--- src/views/EventDescription.jsx
+++ src/views/EventDescription.jsx
@@ -5,11 +5,27 @@ const EventDescription = () =>{
         <div>
       
             <div className="flex flex-col lg:flex-col bg-black stretch-to-screen text-white">
-            <div class="main text-5xl lg:text-7xl flex justify-center">
-                {/* <span class="webdev">FutsalCup mukkiyam bigilae</span> */}
-                    
-                <span class="socod">TEXT ldhalhfklhalkfhalh EFFECT</span>
+            <div class="main text-5xl lg:my-10 lg:text-6xl ml-5 my-5 lg:flex lg:justify-center">
+                <span class="webdev">FutsalCup mukkiyam bigilae</span>
             </div>
+            <div id="description">
+                <div className="main">
+                    <span className="socod text-4xl ml-10">Description</span>
+                </div>
+                <div className="lg:ml-24 mt-4 lg:mr-96">
+                    <p className=" text-2xl leading-9">A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. Though not required by the orthographic conventions of any language with a writing system, paragraphs are a conventional means of organizing extended segments of prose. Wikipedia</p>    
+                </div>
+            </div>
+            <div id="rules">
+                <div className="main">
+                    <span className="socod text-4xl ml-10">Description</span>
+                </div>
+                <div className="lg:ml-24 mt-4 lg:mr-96">
+                    <p className=" text-2xl leading-9">A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. Though not required by the orthographic conventions of any language with a writing system, paragraphs are a conventional means of organizing extended segments of prose. Wikipedia</p>    
+                </div>
+            </div>
+            
+            
             
                   <p className="text-white">text</p>
             </div>
