cff7b2dfd1cffa979ca06513aae43d04a6f1b7b0
KESHAVBERG <kingkeshav7@gmail.com>
2023-03-05 10:12:24 +0530

    added decoration to events

--- src/components/Events/EventCard.jsx
+++ src/components/Events/EventCard.jsx
@@ -20,11 +20,14 @@ const EventCard = ({ title, imgUrl }) => {
                 className="h-60 w-60 rounded-md bg-slate-300 m-4 hover:!scale-105 duration-500 bg-blend-darken	hover:bg-blend-normal box lg:my-10 lg:text-6xl ml-5 lg:flex text-white hover:text-black justify-items-start	" style={
                     myStyle
                 }>           
-                <p className="text-3xl p-2 font-bold left" style={{marginBottom:"290px"}}>{title}</p>
+                <div class="grid  grid-flow-row">
+                        <p className="text-3xl m-3 font-bold	">{title}</p>
+                        <hr class="ml-10 lg:ml-24 border-0 h-1 w-20 mt-2 bg-[#b718f1]                         row-end-1	drop-shadow-lg"></hr>
+
+                    </div>
             </motion.div>
 
-            <div className="overlay">
-            </div>
+            <div className="overlay"></div>
         </div>
     )
 };
