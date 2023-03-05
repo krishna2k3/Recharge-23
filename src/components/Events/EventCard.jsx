c665b58d0992875f0028ae8315d635a2a5180163
manosanjay-g <manosanjay.web@gmail.com>
2023-03-05 10:06:44 +0530

    small tweaks

--- src/components/Events/EventCard.jsx
+++ src/components/Events/EventCard.jsx
@@ -1,32 +1,34 @@
 import { motion } from "framer-motion";
-import './style.css'
+import "./style.css";
 // import dance from './Home/Images/'
 const EventCard = ({ title, imgUrl }) => {
-    const myStyle = {
-        backgroundImage:
-            `url('${imgUrl}')`,
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center', /* Center the image */
-        backgroundSize: "cover",
+  const myStyle = {
+    backgroundImage: `url('${imgUrl}')`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center" /* Center the image */,
+    backgroundSize: "cover",
+  };
+  return (
+    <div className="m-4">
+      <motion.div
+        layout
+        animate={{ opacity: 1, scale: 1 }}
+        initial={{ opacity: 0, scale: 0 }}
+        exit={{ opacity: 0, scale: 0 }}
+        className="h-80 w-80 rounded-md bg-slate-300 hover:!scale-105 duration-500 bg-blend-darken	hover:bg-blend-normal box  lg:text-6xl lg:flex text-white hover:text-black justify-items-start	"
+        style={myStyle}
+      >
+        <p
+          className="text-3xl p-2 font-bold left"
+          style={{ marginBottom: "290px" }}
+        >
+          {title}
+        </p>
+      </motion.div>
 
-    };
-    return (
-        <div className="parent-container ">
-            <motion.div
-                layout
-                animate={{ opacity: 1, scale: 1 }}
-                initial={{ opacity: 0, scale: 0 }}
-                exit={{ opacity: 0, scale: 0 }}
-                className="h-60 w-60 rounded-md bg-slate-300 m-4 hover:!scale-105 duration-500 bg-blend-darken	hover:bg-blend-normal box lg:my-10 lg:text-6xl ml-5 lg:flex text-white hover:text-black justify-items-start	" style={
-                    myStyle
-                }>           
-                <p className="text-3xl p-2 font-bold left" style={{marginBottom:"290px"}}>{title}</p>
-            </motion.div>
-
-            <div className="overlay">
-            </div>
-        </div>
-    )
+      <div className="overlay"></div>
+    </div>
+  );
 };
 
 export default EventCard;
--- src/views/Events.jsx
+++ src/views/Events.jsx
@@ -1,58 +1,121 @@
 import { useState } from "react";
 import EventCard from "../components/Events/EventCard";
 import { motion, AnimatePresence } from "framer-motion";
+import "../components/Common/neonfont.css";
 
 const Events = () => {
-    const events = [
-    { title: "Dance", category: "dance", imgUrl:"https://cdn.dribbble.com/users/789882/screenshots/17267379/media/b450b2f1bde14856e9de0dc13f39e095.png?compress=1&resize=1000x750&vertical=top" },
-    { title: "Music", category: "sing",imgUrl:"https://cdn.dribbble.com/users/572344/screenshots/18178485/media/8d74245cebe09ccc4580b7be5d5dd4de.png?compress=1&resize=1000x750&vertical=top" },
-    { title: "Online Games", category: "game", imgUrl:"https://cdn.dribbble.com/userupload/4399141/file/original-e0e217acf88e32b73591e4fc73fd4447.png?compress=1&resize=1200x899" },
-       
-    { title: "Darama", category: "darama", imgUrl:"https://cdn.dribbble.com/users/458522/screenshots/15007930/media/4415b0e2c5f4f07f057fd2cc969ac655.jpg?compress=1&resize=1000x750&vertical=top" },
-    { title: "Fashion", category: "Fashion",imgUrl:"https://cdn.dribbble.com/userupload/2922074/file/original-ac96fdca5173952de0a4d50a0d06e431.jpg?compress=1&resize=1024x768" },
-    { title: "Entertainment", category: "game", imgUrl:"https://cdn.dribbble.com/userupload/4399141/file/original-e0e217acf88e32b73591e4fc73fd4447.png?compress=1&resize=1200x899" },
-       
-    { title: "Photography", category: "dance", imgUrl:"https://cdn.dribbble.com/users/789882/screenshots/17267379/media/b450b2f1bde14856e9de0dc13f39e095.png?compress=1&resize=1000x750&vertical=top" },
-    { title: "Film", category: "sing",imgUrl:"https://cdn.dribbble.com/users/572344/screenshots/18178485/media/8d74245cebe09ccc4580b7be5d5dd4de.png?compress=1&resize=1000x750&vertical=top" },
-    { title: "Arts", category: "game", imgUrl:"https://cdn.dribbble.com/userupload/4399141/file/original-e0e217acf88e32b73591e4fc73fd4447.png?compress=1&resize=1200x899" },
-     
-    { title: "Sports", category: "dance", imgUrl:"https://cdn.dribbble.com/users/789882/screenshots/17267379/media/b450b2f1bde14856e9de0dc13f39e095.png?compress=1&resize=1000x750&vertical=top" },
-    { title: "Literature", category: "sing",imgUrl:"https://cdn.dribbble.com/users/572344/screenshots/18178485/media/8d74245cebe09ccc4580b7be5d5dd4de.png?compress=1&resize=1000x750&vertical=top" },
-    { title: "Quiz", category: "game", imgUrl:"https://cdn.dribbble.com/userupload/4399141/file/original-e0e217acf88e32b73591e4fc73fd4447.png?compress=1&resize=1200x899" },
-       
-    { title: "On Stage", category: "dance", imgUrl:"https://cdn.dribbble.com/users/789882/screenshots/17267379/media/b450b2f1bde14856e9de0dc13f39e095.png?compress=1&resize=1000x750&vertical=top" },
-    
-    ]
+  const events = [
+    {
+      title: "Dance",
+      category: "dance",
+      imgUrl:
+        "https://cdn.dribbble.com/users/789882/screenshots/17267379/media/b450b2f1bde14856e9de0dc13f39e095.png?compress=1&resize=1000x750&vertical=top",
+    },
+    {
+      title: "Music",
+      category: "sing",
+      imgUrl:
+        "https://cdn.dribbble.com/users/572344/screenshots/18178485/media/8d74245cebe09ccc4580b7be5d5dd4de.png?compress=1&resize=1000x750&vertical=top",
+    },
+    {
+      title: "Online Games",
+      category: "game",
+      imgUrl:
+        "https://cdn.dribbble.com/userupload/4399141/file/original-e0e217acf88e32b73591e4fc73fd4447.png?compress=1&resize=1200x899",
+    },
 
-    const [filtered, setFiltered] = useState(events);
+    {
+      title: "Darama",
+      category: "darama",
+      imgUrl:
+        "https://cdn.dribbble.com/users/458522/screenshots/15007930/media/4415b0e2c5f4f07f057fd2cc969ac655.jpg?compress=1&resize=1000x750&vertical=top",
+    },
+    {
+      title: "Fashion",
+      category: "Fashion",
+      imgUrl:
+        "https://cdn.dribbble.com/userupload/2922074/file/original-ac96fdca5173952de0a4d50a0d06e431.jpg?compress=1&resize=1024x768",
+    },
+    {
+      title: "Entertainment",
+      category: "game",
+      imgUrl:
+        "https://cdn.dribbble.com/userupload/4399141/file/original-e0e217acf88e32b73591e4fc73fd4447.png?compress=1&resize=1200x899",
+    },
 
-    const filterEvents = (category) => {
-        if (category === "all") {
-            setFiltered(events);
-        } else {
-            let newEvents = events.filter((e) => e.category === category);
-            setFiltered(newEvents);
-        }
-    };
+    {
+      title: "Photography",
+      category: "dance",
+      imgUrl:
+        "https://cdn.dribbble.com/users/789882/screenshots/17267379/media/b450b2f1bde14856e9de0dc13f39e095.png?compress=1&resize=1000x750&vertical=top",
+    },
+    {
+      title: "Film",
+      category: "sing",
+      imgUrl:
+        "https://cdn.dribbble.com/users/572344/screenshots/18178485/media/8d74245cebe09ccc4580b7be5d5dd4de.png?compress=1&resize=1000x750&vertical=top",
+    },
+    {
+      title: "Arts",
+      category: "game",
+      imgUrl:
+        "https://cdn.dribbble.com/userupload/4399141/file/original-e0e217acf88e32b73591e4fc73fd4447.png?compress=1&resize=1200x899",
+    },
 
-    return <div id="events">
+    {
+      title: "Sports",
+      category: "dance",
+      imgUrl:
+        "https://cdn.dribbble.com/users/789882/screenshots/17267379/media/b450b2f1bde14856e9de0dc13f39e095.png?compress=1&resize=1000x750&vertical=top",
+    },
+    {
+      title: "Literature",
+      category: "sing",
+      imgUrl:
+        "https://cdn.dribbble.com/users/572344/screenshots/18178485/media/8d74245cebe09ccc4580b7be5d5dd4de.png?compress=1&resize=1000x750&vertical=top",
+    },
+    {
+      title: "Quiz",
+      category: "game",
+      imgUrl:
+        "https://cdn.dribbble.com/userupload/4399141/file/original-e0e217acf88e32b73591e4fc73fd4447.png?compress=1&resize=1200x899",
+    },
 
-        <div id="content" className="flex flex-col mt-20">
-        <p className="ml-10 mt-16 text-7xl lg:mt-16 lg:ml-24 text-white ">Events</p>    
-                    <hr class="ml-10 lg:ml-24 border-0 h-1 w-20 mt-2 bg-[#b718f1] drop-shadow-lg"></hr>
-                    <hr class="ml-24 lg:ml-36 w-20 h-1 border-0 mt-4 bg-[#b718f1]"></hr>
-       
+    {
+      title: "On Stage",
+      category: "dance",
+      imgUrl:
+        "https://cdn.dribbble.com/users/789882/screenshots/17267379/media/b450b2f1bde14856e9de0dc13f39e095.png?compress=1&resize=1000x750&vertical=top",
+    },
+  ];
 
-            <motion.div layout id="event-cards" className="flex flex-row flex-wrap justify-center">
-                <AnimatePresence>
-                    {filtered.map((e) => {
-                        return <EventCard key={e.title} title={e.title} imgUrl={e.imgUrl}></EventCard>
-                    })}
-                </AnimatePresence>
-            </motion.div>
-
+  return (
+    <div id="events">
+      <div id="content" className="flex flex-col items-center py-10 gap-y-4">
+        <div class="main text-6xl">
+          <span class="webdev">EVENTS</span>
         </div>
+
+        <motion.div
+          layout
+          id="event-cards"
+          className="flex flex-row flex-wrap justify-center"
+        >
+          <AnimatePresence>
+            {events.map((e) => {
+              return (
+                <EventCard
+                  key={e.title}
+                  title={e.title}
+                  imgUrl={e.imgUrl}
+                ></EventCard>
+              );
+            })}
+          </AnimatePresence>
+        </motion.div>
+      </div>
     </div>
+  );
 };
 
 export default Events;
