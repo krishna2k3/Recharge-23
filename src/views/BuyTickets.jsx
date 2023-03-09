662d8f32333fe1f81e175729519e06350140b048
c0d3-br3ak9r <103585603+c0d3-br3ak9r@users.noreply.github.com>
2023-03-09 11:44:20 +0530

    Updated Buy Tickets page

--- src/views/BuyTickets.jsx
+++ src/views/BuyTickets.jsx
@@ -40,14 +40,48 @@ const BuyTickets = ()=>{
                     <span class="webdev">Tickets</span>
 
                 </div>
-                <div className='mt-16 flex flex-row flex-wrap'>
+                <div className='mt-16 lg:grid lg:grid-cols-2 flex flex-col'>
                     <div 
-                        className="border-white h-80 w-80 m-6 rounded-md bg-slate-900 hover:!scale-105 duration-500 hover:bg-blend-normal box lg:text-6xl lg:flex text-white justify-center items-center">
-                        Hello
+                        className="border-2 border-[#BBC2CC] p-4 lg:m-12 m-4 rounded-md box !bg-[#BBC2CC40] hover:!bg-[#BBC2CC60] hover:!scale-105 duration-500 lg:flex text-white justify-center flex-col lg:basis-1/2">
+                        <p className='text-4xl m-3 text-white'>Standard</p>
+                        <ul className='list-disc'>
+                            <li className='m-3 ml-6'>
+                                Gain exclusive entry to over 75 exciting events with our all-access pass, including highly anticipated proshows.
+                            </li>
+                            <li className='m-3 ml-6'>
+                                Be a part of the concert action with standard tickets - entry is based on first come, first serve!
+                            </li>
+                            <li className='m-3 ml-6'>
+                                Get your tickets now and enjoy the concert with your friends - each standard ticket admits one concertgoer!
+                            </li>
+                            <li className='m-3 ml-6'>
+                                Don't miss out on the chance to see your favorite artists live - standard tickets are going fast!
+                            </li>
+                            <li className='m-3 ml-6'>
+                                Experience the thrill of live music with standard tickets and be a part of the unforgettable concert atmosphere!
+                            </li>
+                        </ul>
                     </div>
                     <div 
-                        className="h-80 w-80 m-6 rounded-md bg-slate-900 hover:!scale-105 duration-500 hover:bg-blend-normal box lg:text-6xl lg:flex text-white justify-center items-center">
-                        Hello
+                        className="border-2 border-[#FFD700] p-4 lg:m-12 m-4 rounded-md !bg-[#FFD70020] hover:!bg-[#FFD70040] hover:!scale-105 duration-500 hover:bg-blend-normal box lg:flex text-white justify-center flex-col lg:basis-1/2">
+                        <p className='text-4xl m-3 text-[#FFD700]'>Premium</p>
+                        <ul className='list-disc'>
+                            <li className='m-3 ml-6'>
+                                Gain exclusive entry to over 75 exciting events with our all-access pass, including highly anticipated proshows.
+                            </li>
+                            <li className='m-3 ml-6'>
+                                Elevate your concert experience with premium tickets and enjoy exclusive access to the fanpit only for premium ticket holders!
+                            </li>
+                            <li className='m-3 ml-6'>
+                                Get up close and in the center of the action with your favorite artists! With premium standing tickets, it's first come, first serve - so don't wait!
+                            </li>
+                            <li className='m-3 ml-6'>
+                                Treat yourself to the ultimate concert experience with premium tickets priced at 1.5k per day!
+                            </li>
+                            <li className='m-3 ml-6'>
+                                Each premium ticket admits one reveller to the concert - don't miss out on this incredible opportunity!
+                            </li>
+                        </ul>
                     </div>
                 </div>
         </div>
