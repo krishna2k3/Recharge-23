eaaaccc6637154d1eea443e6b0ce1a2f09e10416
c0d3-br3ak9r <103585603+c0d3-br3ak9r@users.noreply.github.com>
2023-03-09 15:08:39 +0530

    Updated spotlight

--- src/components/Home/Proshows/Spotlight.jsx
+++ src/components/Home/Proshows/Spotlight.jsx
@@ -1,25 +1,24 @@
-import "./Proshow.css";
+/* import "./Proshow.css"; */
 
 const Spotlight = () => {
   return (
     <div
       id="proshows"
-      className="flex flex-col  text-white items-center px-5 stretch-to-screen mt-8"
+      className="flex flex-col  text-white items-center px-5 strech-to-screen mt-8"
     >
       <div className="main text-6xl md:7xl mb-5 ">
         <span className="webdev text-left">SPOTLIGHT</span>
       </div>
-      <div className="proshow-cards">
-        <div className="proshow-card lg:scale-90 relative flex flex-col justify-end items-center rounded-xl">
+        {/* <div className="proshow-card lg:scale-90 relative flex flex-col justify-end items-center rounded-xl">
           <img
             className="-z-10 absolute proshows-bg rounded-xl"
             alt="background-jonita"
             src={require("../../../assets/bg-2.webp")}
           />
           <img
-            src={require("../../../assets/raina-bg.png")}
+            src={require("../../../assets/raina-transformed_new.png")}
             alt="Jonita"
-            className="z-0 rounded-xl"
+            className="z-0 rounded-xl object-contain"
           />
           <div className="z-10 gap-y-1 flex flex-col absolute my-4">
             <h1 className="  text-3xl text-center font-bold tracking-widest content">
@@ -29,10 +28,28 @@ const Spotlight = () => {
               
             </h1>
           </div>
+        </div> */}
+        <div className="proshow-card relative flex  flex-col justify-end items-center rounded-xl">
+          <img
+            className="-z-10 absolute proshows-bg rounded-xl"
+            alt="background-jonita"
+            src={require("../../../assets/bg-2.webp")}
+          />
+          <img
+            src={require("../../../assets/jonita.png")}
+            alt="Jonita"
+            className="z-0 rounded-xl"
+          />
+          <div className="z-10 gap-y-1 flex flex-col absolute my-4">
+            <h1 className="  text-3xl text-center font-bold tracking-widest content">
+              JONITA GANDHI
+            </h1>
+            <h1 className="  text-md text-center font-bold tracking-widest content">
+              DAY 1
+            </h1>
+          </div>
         </div>
-        
       </div>
-    </div>
   );
 };
 
