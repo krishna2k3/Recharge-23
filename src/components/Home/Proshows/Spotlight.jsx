05e569ba788776322d2fc29e9a40d7258ac3b6b6
manosanjay-g <manosanjay.web@gmail.com>
2023-03-09 19:32:20 +0530

    small tweak

--- src/components/Home/Proshows/Spotlight.jsx
+++ src/components/Home/Proshows/Spotlight.jsx
@@ -2,41 +2,44 @@
 
 const Spotlight = () => {
   return (
-    <div
-      id="proshows"
-      className="flex flex-col  text-white items-center px-5 strech-to-screen mt-8"
-    >
-      <div className="main text-6xl md:7xl mb-5 ">
+    <div id="spotlight" className="flex flex-col  text-white items-center px-5 stretch-to-screen mt-8">
+      <div className="main text-6xl md:7xl mb-5 text-white">
         <span className="webdev text-left">SPOTLIGHT</span>
       </div>
-        {/* <div className="proshow-card lg:scale-90 relative flex flex-col justify-end items-center rounded-xl">
+      <div className="proshow-cards">
+        <div className="proshow-card relative flex flex-col justify-end items-center ">
           <img
             className="-z-10 absolute proshows-bg rounded-xl"
-            alt="background-jonita"
-            src={require("../../../assets/bg-2.webp")}
+            alt="background-?3"
+            src="http://content.wcsh6.com/photo/2017/09/26/unknown%20suspect_1506420048586_11238702_ver1.0.png"
           />
-          <img
-            src={require("../../../assets/raina-transformed_new.png")}
+          {/* <img
+            className="-z-10 absolute proshows-bg"
+            src={require("../../../assets/pro-shows-bg.jpg")}
+
+          /> */}
+          {/* <img
+            src={require("../../../assets/jonita.png")}
             alt="Jonita"
-            className="z-0 rounded-xl object-contain"
-          />
+            className="z-0"
+          /> */}
           <div className="z-10 gap-y-1 flex flex-col absolute my-4">
             <h1 className="  text-3xl text-center font-bold tracking-widest content">
-              SURESH RAINA
+              ??
             </h1>
             <h1 className="  text-md text-center font-bold tracking-widest content">
-              
+              DAY 1
             </h1>
           </div>
-        </div> */}
-        <div className="proshow-card relative flex  flex-col justify-end items-center rounded-xl">
+        </div>
+        <div className="proshow-card relative flex flex-col justify-end items-center rounded-xl">
           <img
             className="-z-10 absolute proshows-bg rounded-xl"
             alt="background-jonita"
             src={require("../../../assets/bg-2.webp")}
           />
           <img
-            src={require("../../../assets/raina-transformed_new.png")}
+            src={require("../../../assets/raina-transformed.png")}
             alt="Jonita"
             className="z-0 rounded-xl"
           />
@@ -44,12 +47,39 @@ const Spotlight = () => {
             <h1 className="  text-3xl text-center font-bold tracking-widest content">
               SURESH RAINA
             </h1>
-            {/* <h1 className="  text-md text-center font-bold tracking-widest content">
-              DAY 1
-            </h1> */}
+            <h1 className="  text-md text-center font-bold tracking-widest content">
+              FORMER INDIAN CRICKETER
+            </h1>
+          </div>
+        </div>
+
+        <div className="proshow-card relative flex flex-col justify-end items-center ">
+          <img
+            className="-z-10 absolute proshows-bg rounded-xl"
+            alt="background-?3"
+            src="http://content.wcsh6.com/photo/2017/09/26/unknown%20suspect_1506420048586_11238702_ver1.0.png"
+          />
+          {/* <img
+            className="-z-10 absolute proshows-bg"
+            src={require("../../../assets/pro-shows-bg.jpg")}
+
+          /> */}
+          {/* <img
+            src={require("../../../assets/jonita.png")}
+            alt="Jonita"
+            className="z-0"
+          /> */}
+          <div className="z-10 gap-y-1 flex flex-col absolute my-4">
+            <h1 className="  text-3xl text-center font-bold tracking-widest content">
+              ??
+            </h1>
+            <h1 className="  text-md text-center font-bold tracking-widest content">
+              DAY 3
+            </h1>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
