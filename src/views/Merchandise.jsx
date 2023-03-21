c7ec0d281dcc98c7272f889d2c9821cf15c11186
Subze23 <subzesubhashsanth@gmail.com>
2023-03-21 13:54:42 +0530

    merch done

--- src/views/Merchandise.jsx
+++ src/views/Merchandise.jsx
@@ -4,9 +4,9 @@ import "./css/Merchandise.css";
 
 const Merchandise = () => {
 
-  const slides = [require("../assets/merchandise/Tshirt_Front.png"),
-  require("../assets/merchandise/Tshirt_Side.png"),
-  require("../assets/merchandise/Tshirt_Back.png")]
+  const slides = [require("../assets/merchandise/front.webp"),
+  require("../assets/merchandise/side.webp"),
+  require("../assets/merchandise/back.webp")]
 
   const [current, setCurrent] = useState(0)
   const [style1, setStyle1] = useState('active')
@@ -16,14 +16,6 @@ const Merchandise = () => {
   let i = 0;
 
   const mainImg = useRef(null);
-
-  useEffect(() => {
-    setInterval(() => {
-      i = (i+1)%3;
-      console.log("Auto changing : " + i);
-      changeCurrent(i);
-    }, 10000)
-  }, []);
 
   const changeCurrent = (idx) => {
     mainImg.current.classList.remove('show');
@@ -45,7 +37,7 @@ const Merchandise = () => {
       }
       mainImg.current.classList.remove('hide');
       mainImg.current.classList.add('show');
-    }, 2000);
+    }, 1500);
     
   }
 
@@ -57,7 +49,7 @@ const Merchandise = () => {
           <span class="socod">MERCHANDISE</span>
         </div>
         <div id="main-content" className="flex flex-col lg:flex-row">
-          <div id="images" className="flex flex-col justify-start basis-3/4">
+          <div id="images" className="flex flex-col justify-start basis-3/5">
             <div className="flex justify-center">
               <img ref={mainImg} className="main-img" src={slides[current]} alt="" />
             </div>
@@ -67,31 +59,45 @@ const Merchandise = () => {
               <img className={style3} src={slides[2]} alt="" onClick={() => changeCurrent(2)}/>
             </div>
           </div>
-          <div id="desc" className="flex flex-col basis-1/4">
+          <div id="desc" className="flex flex-col basis-2/5">
             <div className="flex flex-row ">
               <div className="main mt-4">
-                <span className="socod text-xl ">Time:</span>
+                <span className="socod text-xl ">Price:</span>
               </div>
               <div className="ml-2 mt-4">
-                <p className=" text-xl text-white">something</p>
+                <p className=" text-xl text-white">Rs. 350</p>
               </div>
             </div>
             <div className="flex flex-row ">
               <div className="main mt-4">
-                <span className="socod text-xl ">Time:</span>
+                <span className="socod text-xl ">Sizes:</span>
               </div>
               <div className="ml-2 mt-4">
-                <p className=" text-xl text-white">something</p>
+                <p className=" text-xl text-white">S | M | L | XL</p>
               </div>
             </div>
             <div className="flex flex-row ">
               <div className="main mt-4">
-                <span className="socod text-xl ">Time:</span>
+                <span className="socod text-xl ">About:</span>
               </div>
               <div className="ml-2 mt-4">
-                <p className=" text-xl text-white">something</p>
+                <p className=" text-xl text-white">Get ready to rock with our Recharge Fest t-shirt! Featuring a vibrant design inspired by the Recharge Fest's theme, this shirt is perfect for music lovers and Fest-goers alike. Made from high-quality materials, this shirt is both comfortable and durable. Show off your love for the Fest and look great while doing it with our Recharge Fest t-shirt!</p>
               </div>
             </div>
+           
+            
+            <div className="flex justify-center">
+            <button
+            type="button"
+            onClick={() => window.open("http://rec.rechargefest.org/merch", "_self")}
+            className="mt-5 px-4 py-2 text-white items-center hover:text-white hover:bg-[#0099ff8a]  duration-200 rounded-md font-medium hover:duration-500 px neon-btn-socod w-52"
+          >
+            <div class="main text-md">
+              <span class="socod text-left">BUY NOW</span>
+            </div>
+          </button>
+            </div>
+
           </div>
         </div>
       </div>
